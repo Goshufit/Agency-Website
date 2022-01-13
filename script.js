8cf6fc0752746e5d537985fb781959dba4828f8a
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-01-12 21:58:06 -0500

    Add JavaScript toggle to menu icon add styling to menu and social media icons move email icon to bottom left of page add pseudo  class to shift color of menu text

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle')
+const toggle = document.querySelector('.navigation')
+toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
